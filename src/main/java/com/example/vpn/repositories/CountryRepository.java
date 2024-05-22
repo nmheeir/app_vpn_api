1efea5bc70b8deea6b24a46627ebb249bc9339bd
nmheir <nmh7624@gmail.com>
2024-05-22 14:02:12 +0700

    sửa lỗi xóa country

--- src/main/java/com/example/vpn/repositories/CountryRepository.java
+++ src/main/java/com/example/vpn/repositories/CountryRepository.java
@@ -8,5 +8,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 @EnableJpaRepositories
 public interface CountryRepository extends JpaRepository<Country, Integer> {
-    void deleteCountryById();
 }
